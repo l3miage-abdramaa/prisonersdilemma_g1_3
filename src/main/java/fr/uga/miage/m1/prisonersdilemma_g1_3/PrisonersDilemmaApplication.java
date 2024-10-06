eb3689b023e13bf78ba6c821e9780cb1d8cb220d
abdramaa <abakar.abdraman@etu.univ-grenoble-alpes.fr>
2024-10-06 21:25:07 +0200

    Initial commit with project setup

---
+++ src/main/java/fr/uga/miage/m1/prisonersdilemma_g1_3/PrisonersDilemmaApplication.java
@@ -0,0 +1,13 @@
+package fr.uga.miage.m1.prisonersdilemma_g1_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrisonersDilemmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrisonersDilemmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/uga/miage/m1/prisonersdilemma_g1_3/PrisonersDilemmaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uga.miage.m1.prisonersdilemma_g1_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonersDilemmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
